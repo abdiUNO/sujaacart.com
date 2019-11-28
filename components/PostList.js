fa8021b18481b9362ff6afef26d2c29b1d5dec3a
abdiUNO <amahamed@unomaha.edu>
2019-11-28 02:03:44 -0600

    updated config

--- components/PostList.js
+++ components/PostList.js
@@ -3,6 +3,23 @@ import './comic.css';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
+const BLOG_POSTS_PATH = '../../content/posts';
+
+const importBlogPosts = async () => {
+  // https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
+  // second flag in require.context function is if subdirectories should be searched
+  const markdownFiles = require
+    .context(BLOG_POSTS_PATH, false, /\.md$/)
+    .keys()
+    .map(relativePath => relativePath.substring(2));
+  return Promise.all(
+    markdownFiles.map(async path => {
+      const markdown = await import(`${BLOG_POSTS_PATH}/${path}`);
+      return { ...markdown, slug: path.substring(0, path.length - 3) };
+    })
+  );
+};
+
 let comics = [];
 
 for (var i = 0; i < 15; i++) {
@@ -27,33 +44,44 @@ const postVariants = {
   }
 };
 
-const PostList = () => (
-  <motion.div initial="initial" animate="enter" exit="exit">
-    <div className="row">
-      <div className="gallery" id="gallery">
-        {comics.map((_comic, index) => (
-          <div key={_comic.id} className="mb-3 pics animation all 2 mx-3 py-2">
-            <motion.div variants={postVariants}>
-              <Link href="/posts/[post]" as={`/posts/${_comic.id}`}>
-                <a className="text-decoration-none">
-                  <motion.div
-                    whileHover="hover"
-                    variants={{ hover: { scale: 0.96 } }}>
-                    <img
-                      className="img-fluid"
-                      src={`/img/comic_0${_comic.id}.jpg`}
-                      alt="Card image cap"
-                    />
-                  </motion.div>
-                  <p className="img-date">November 19th, 2019</p>
-                </a>
-              </Link>
-            </motion.div>
-          </div>
-        ))}
+const PostList = ({ postsList }) => {
+  console.log(postsList);
+  return (
+    <motion.div initial="initial" animate="enter" exit="exit">
+      <div className="row">
+        <div className="gallery" id="gallery">
+          {comics.map((_comic, index) => (
+            <div
+              key={_comic.id}
+              className="mb-3 pics animation all 2 mx-3 py-2">
+              <motion.div variants={postVariants}>
+                <Link href="/posts/[post]" as={`/posts/${_comic.id}`}>
+                  <a className="text-decoration-none">
+                    <motion.div
+                      whileHover="hover"
+                      variants={{ hover: { scale: 0.96 } }}>
+                      <img
+                        className="img-fluid"
+                        src={`/img/comic_0${_comic.id}.jpg`}
+                        alt="Card image cap"
+                      />
+                    </motion.div>
+                    <p className="img-date">November 19th, 2019</p>
+                  </a>
+                </Link>
+              </motion.div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  </motion.div>
-);
+    </motion.div>
+  );
+};
+
+PostList.getInitialProps = async () => {
+  const postsList = await importBlogPosts();
+
+  return { postsList };
+};
 
 export default PostList;
--- next.config.js
+++ next.config.js
@@ -1,11 +1,40 @@
 // next.config.js
+const fs = require('fs');
+const posts = './content/posts';
+
 const withCSS = require('@zeit/next-css');
+
+const getPathsForPosts = () => {
+  return fs
+    .readdirSync(posts)
+    .map(blogName => {
+      const trimmedName = blogName.substring(0, blogName.length - 3);
+      return {
+        [`/blog/post/${trimmedName}`]: {
+          page: '/blog/post/[slug]',
+          query: {
+            slug: trimmedName
+          }
+        }
+      };
+    })
+    .reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    }, {});
+};
+
 module.exports = withCSS({
-  webpack: cfg => {
-    cfg.module.rules.push({
+  webpack: configuration => {
+    configuration.module.rules.push({
       test: /\.md$/,
       use: 'frontmatter-markdown-loader'
     });
-    return cfg;
+    return configuration;
+  },
+  async exportPathMap(defaultPathMap) {
+    return {
+      ...defaultPathMap,
+      ...getPathsForPosts()
+    };
   }
 });
