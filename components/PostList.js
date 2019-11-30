0518501a7d3015a6f7d54c8cb50f2f025e07606c
abdiUNO <amahamed@unomaha.edu>
2019-11-30 05:38:00 -0600

    updated site

--- components/PostList.js
+++ components/PostList.js
@@ -3,6 +3,11 @@ import './comic.css';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import Masonry from 'react-masonry-component';
+import {
+  LazyLoadImage,
+  trackWindowScroll
+} from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const masonryOptions = {
   transitionDuration: 0,
@@ -13,12 +18,12 @@ const masonryOptions = {
 const imagesLoadedOptions = { background: '.my-bg-image-el' };
 
 const postVariants = {
-  initial: { scale: 0.95, y: 30, opacity: 0 },
+  initial: { scale: 0.95, y: -15, opacity: 0 },
   enter: {
     scale: 1,
     y: 0,
     opacity: 1,
-    transition: { duration: 0.5, ease: [0.48, 0.15, 0.25, 0.96] }
+    transition: { duration: 0.3, ease: [0.48, 0.15, 0.25, 0.96] }
   },
   exit: {
     scale: 0.6,
@@ -28,50 +33,55 @@ const postVariants = {
   }
 };
 
-const profileVariants = {
-  initial: { scale: 0.96, y: 30, opacity: 0 },
-  enter: {
-    scale: 1,
-    y: 0,
-    opacity: 1,
-    transition: { duration: 0.5, ease: [0.48, 0.15, 0.25, 0.96] }
-  },
-  exit: {
-    scale: 0.6,
-    y: 100,
-    opacity: 0,
-    transition: { duration: 0.2, ease: [0.48, 0.15, 0.25, 0.96] }
-  }
-};
+const placeholder = (
+  <div className="photo-placeholder">
+    <style jsx>{`
+      .photo-placeholder {
+        height: 300px;
+        width: 100vw;
+      }
+    `}</style>
+  </div>
+);
 
-function PostList({ posts }) {
-  const childElements = posts.map((_comic, index) => (
-    <div
-      key={_comic.id}
-      id={index}
-      className="mb-3 pics animation col-md-4 col-sm-6 col-xs-12">
-      <motion.div variants={postVariants}>
-        <Link href={`/posts/${_comic.id}`}>
-          <a className="text-decoration-none">
-            <motion.div
-              whileHover="hover"
-              variants={{
-                hover: { scale: 0.96, bounceDamping: 8 }
-              }}>
-              <img
-                className="img-fluid"
-                src={`${_comic.image}?nf_resize=fit&w=400`}
-                alt={`Post ${_comic.date}`}
-                loading="auto"
-              />
-            </motion.div>
-            <p className="img-date">November 19th, 2019</p>
-          </a>
-        </Link>
-      </motion.div>
-    </div>
-  ));
+const Gallery = ({ posts, scrollPosition }) => (
+  <div>
+    {posts.map((_comic, index) => (
+      <div
+        key={_comic.id}
+        id={index}
+        className="mb-3 pics animation col-md-4 col-sm-6 col-xs-12">
+        <motion.div variants={postVariants}>
+          <Link prefetch={false} href={`/posts/${_comic.id}`}>
+            <a className="text-decoration-none">
+              <motion.div
+                whileHover="hover"
+                variants={{
+                  hover: { scale: 0.96, bounceDamping: 8 }
+                }}>
+                <LazyLoadImage
+                  className="img-fluid"
+                  alt={`Post ${_comic.date}`}
+                  scrollPosition={scrollPosition}
+                  effect="blur"
+                  width={'100%'}
+                  placeholder={placeholder}
+                  height={'auto'}
+                  src={`${_comic.image}?nf_resize=fit&w=400`} // use normal <img> attributes as props
+                />
+                <p className="img-date">November 19th, 2019</p>
+              </motion.div>
+            </a>
+          </Link>
+        </motion.div>
+      </div>
+    ))}
+  </div>
+);
 
+const GalleryWithScroll = trackWindowScroll(Gallery);
+
+function PostList({ posts, scrollPosition }) {
   return (
     <div style={{ height: '100%' }}>
       <motion.div initial="initial" animate="enter" exit="exit">
@@ -91,7 +101,7 @@ function PostList({ posts }) {
           className={'gallery'} // default ''
           options={masonryOptions} // default {}
           enableResizableChildren={true}>
-          {childElements}
+          <GalleryWithScroll posts={posts} />
         </Masonry>
       </motion.div>
       <style jsx>{`
